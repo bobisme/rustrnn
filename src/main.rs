3428637f416b738eeff29a35fa3685d69d4a18af
Bob <bob@arbor.io>
2017-06-05 02:00:24 -0400

    Bug in here. Many things learned.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 extern crate rand;
 
-use rand::Rng;
 use rand::distributions::{Range,IndependentSample};
 
 mod utils;
@@ -9,14 +8,13 @@ mod utils_tests;
 mod layer;
 mod matrix;
 
-use utils::{sigmoid, sigmoid_derivative};
-use layer::{Layer};
-use matrix::{Matrix};
+use utils::{Sigmoid};
+use matrix::{Matrix, Transpose};
 
 // const LEARNING_RATE: f64 = 0.1;
 const INPUT_SIZE: usize = 2;
 const HIDDEN_SIZE: usize = 16;
-// const OUTPUT_SIZE: u32 = 1;
+const OUTPUT_SIZE: usize = 1;
 const BIT_COUNT: usize = 8;
 
 
@@ -24,44 +22,162 @@ fn get_bits(i: i32) -> Vec<i32> {
     (0..BIT_COUNT).rev().map(|k| (i & (1 << k)) >> k).collect()
 }
 
+fn forward_hidden_layer<'a, 'b>(
+    input: &'a Matrix, hidden_weights: &'a Matrix,
+    previous_hidden: &'b Matrix,
+    recurrent_weights: &'b Matrix) -> Matrix {
+    // hidden layer forward
+    let current_op = match input * hidden_weights {
+        Ok(v) => v, Err(e) => panic!(e),
+    };
+    // recurrent step
+    let recurrent_op = match previous_hidden * recurrent_weights {
+        Ok(v) => v, Err(e) => panic!(e),
+    };
+    (current_op + &recurrent_op).sigmoid()
+}
+
+
+struct IntGenerator {
+    rng: rand::ThreadRng,
+    range: Range<i32>,
+}
+
+impl IntGenerator {
+    pub fn new(from: i32, to: i32) -> IntGenerator {
+        IntGenerator {
+            rng: rand::thread_rng(),
+            range: Range::new(from, to),
+        }
+    }
+    pub fn next(&mut self) -> i32 {
+        let rng = &mut self.rng;
+        self.range.ind_sample(rng)
+    }
+}
+
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let x = 2.0;
-    println!("{}", sigmoid(x));
-    println!("{}", sigmoid_derivative(x));
-    println!("{}", sigmoid_derivative(sigmoid(x)));
-    println!("{}", rng.gen::<f64>());
+    let mut int_gen = IntGenerator::new(0, 128);
 
-    let l = Layer::new(INPUT_SIZE, HIDDEN_SIZE);
-    println!("Layer is {} big", l.size() * l.input_size());
-    println!("thing: {:?}", l.weights);
-    println!("{:?}", Matrix::new(2, 3));
+    let mut hidden_layer_weights = Matrix::new_random(INPUT_SIZE, HIDDEN_SIZE);
+    let mut output_layer_weights = Matrix::new_random(HIDDEN_SIZE, OUTPUT_SIZE);
+    let mut recurrent_layer_weights = Matrix::new_random(
+        HIDDEN_SIZE, HIDDEN_SIZE);
 
-    let int_range = Range::new(0, 128);
+    let mut hidden_layer_update = Matrix::new(INPUT_SIZE, HIDDEN_SIZE);
+    let mut output_layer_update = Matrix::new(HIDDEN_SIZE, OUTPUT_SIZE);
+    let mut recurrent_layer_update = Matrix::new(HIDDEN_SIZE, HIDDEN_SIZE);
 
-    for _ in 0..1 {
-        let a: i32 = int_range.ind_sample(&mut rng);
+    for iteration in 0..1 {
+        let a = int_gen.next();
         let a_bits = get_bits(a);
-        let b: i32 = int_range.ind_sample(&mut rng);
+        let b = int_gen.next();
         let b_bits = get_bits(b);
         let c = a + b;
         let c_bits = get_bits(c);
 
-        // let prediction: [i32; BIT_COUNT] = [0; BIT_COUNT];
-        // let overall_error = 0.0;
+        let mut prediction: [i32; BIT_COUNT] = [0; BIT_COUNT];
+        let mut overall_error: f64 = 0.0;
 
-        // let hidden_layer_deltas = ?;
-        let mut input_layer_history: Vec<Vec<i32>> = Default::default();
-        input_layer_history.push(vec![0, BIT_COUNT as i32]);
+        let mut output_deltas: Vec<Matrix> = vec![];
+        let mut hidden_layer_history: Vec<Matrix> = Default::default();
+        hidden_layer_history.push(Matrix::new(1, HIDDEN_SIZE));
 
-        for (a, b) in a_bits.iter().zip(b_bits.iter()).rev() {
-            println!("a: {}, b: {}", a, b)
+        let bits_iter = (0..BIT_COUNT).rev()
+            .map(|i| (i, a_bits[i], b_bits[i], c_bits[i]));
+
+        for (bit_pos, a_bit, b_bit, c_bit) in bits_iter {
+            println!("a: {}, b: {}", a_bit, b_bit);
+            let input_layer = Matrix::from(
+                1, INPUT_SIZE, &vec![a_bit as f64, b_bit as f64]);
+            let labels = Matrix::from(1, 1, &vec![c_bit as f64]);
+
+            let hidden_layer = forward_hidden_layer(
+                &input_layer, &hidden_layer_weights,
+                hidden_layer_history.last().unwrap(),
+                &recurrent_layer_weights);
+
+            // output layer forward
+            let output_layer_op = match &hidden_layer * &output_layer_weights {
+                Ok(v) => v, Err(e) => panic!(e)
+            };
+            let output_layer = output_layer_op.sigmoid();
+
+            let output_error = labels - &output_layer;
+            overall_error += output_error[(0, 0)].abs();
+
+            // set the predicted bit
+            prediction[bit_pos] = output_layer[(0, 0)].round() as i32;
+
+            // save the deltaas for each timestep
+            let output_derivative = output_layer.sigmoid_derivative();
+            let output_delta = match output_error * &output_derivative {
+                Ok(v) => v, Err(e) => panic!(e),
+            };
+            output_deltas.push(output_delta);
+
+            // store the output of the hidden layer for the next timestep
+            hidden_layer_history.push(hidden_layer.clone());
         }
 
-        println!("{} + {} = {}", a, b, c);
-        println!("{:b} + {:b} = {:b}", a, b, c);
-        println!("{:?} + {:?} = {:?}", a_bits, b_bits, c_bits);
-        println!("{:?}", (23 & (1 << 2)) >> 2);
+        let mut next_hidden_layer_delta = Matrix::new(1, HIDDEN_SIZE);
+        for i in 0..BIT_COUNT {
+            let inputs = Matrix::from(
+                2, 1, &vec![a_bits[i] as f64, b_bits[i] as f64]);
+            let hidden_layer_index = hidden_layer_history.len() - i - 1;
+            let hidden_layer = &hidden_layer_history[hidden_layer_index];
+            let prev_hidden_layer = &hidden_layer_history[
+                hidden_layer_index - 1];
+
+            let output_delta = &output_deltas[output_deltas.len() - i - 1];
+            let recurrent_dot = {
+                let delta = &next_hidden_layer_delta;
+                let weights = &recurrent_layer_weights;
+                let trans = weights.transpose();
+                match delta * trans {
+                    Ok(v) => v, Err(e) => panic!(e),
+                }
+            };
+            let hidden_layer_delta = {
+                let weights = &hidden_layer_weights;
+                let trans = weights.transpose();
+                println!("{:?}\n{:?}", output_delta, trans);
+                let output_delta_dot = match output_delta * trans {
+                    Ok(v) => v, Err(e) => panic!(e)
+                };
+                let hidden_differential = hidden_layer.sigmoid_derivative();
+                (recurrent_dot + &output_delta_dot) * &hidden_differential
+            }.unwrap();
+
+            output_layer_update +=
+                match hidden_layer.transpose() * output_delta {
+                    Ok(v) => v, Err(e) => panic!(e)
+                };
+            recurrent_layer_update +=
+                match prev_hidden_layer.transpose() * &hidden_layer_delta {
+                    Ok(v) => v, Err(e) => panic!(e)
+                };
+            hidden_layer_update +=
+                match inputs.transpose() * &hidden_layer_delta {
+                    Ok(v) => v, Err(e) => panic!(e)
+                };
+
+            next_hidden_layer_delta = hidden_layer_delta;
+        }
+
+        hidden_layer_weights += &hidden_layer_update;
+        output_layer_weights += &output_layer_update;
+        recurrent_layer_weights += &recurrent_layer_update;
+
+        hidden_layer_weights *= 0.0;
+        output_layer_weights *= 0.0;
+        recurrent_layer_weights *= 0.0;
+
+        if iteration % 1000 == 0 {
+            println!("Error: {}", overall_error);
+            println!("Pred: {:?}", prediction);
+            println!("True: {:?}", c_bits);
+        }
     }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,9 +1,9 @@
 extern crate rand;
 
-use std::ops::{Mul, Index};
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, Index};
 use utils::{Sigmoid, sigmoid, sigmoid_derivative};
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Default)]
 pub struct Matrix {
     pub rows: usize,
     pub cols: usize,
@@ -28,21 +28,53 @@ impl Matrix {
         }
     }
 
-    fn from(rows: usize, cols: usize, data: &Vec<f64>) -> Matrix {
+    pub fn from(rows: usize, cols: usize, data: &Vec<f64>) -> Matrix {
         Matrix {
             rows,
             cols,
             data: data.clone(),
         }
     }
+}
 
-    // pub fn exp(&self) -> Matrix {
-    //     Matrix {
-    //         rows: self.rows,
-    //         cols: self.cols,
-    //         data: self.data.clone().iter().map(|x| E.powf(*x)).collect(),
-    //     }
-    // }
+pub trait Transpose {
+    fn transpose(self) -> Matrix;
+}
+
+impl Transpose for Matrix {
+    fn transpose(self) -> Matrix {
+        let mut out = Matrix::new(self.cols, self.rows);
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                let out_index = out.get_index((col, row));
+                out.data[out_index] = self[(row, col)];
+            }
+        }
+        out
+    }
+}
+
+impl<'a> Transpose for &'a Matrix {
+    fn transpose(self) -> Matrix {
+        let mut out = Matrix::new(self.cols, self.rows);
+        for row in 0..self.rows {
+            for col in 0..self.cols {
+                let out_index = out.get_index((col, row));
+                out.data[out_index] = self[(row, col)];
+            }
+        }
+        out
+    }
+}
+
+impl Clone for Matrix {
+    fn clone(&self) -> Matrix {
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: self.data.clone(),
+        }
+    }
 }
 
 trait FlatIndex<Idx>
@@ -81,21 +113,104 @@ impl Mul<f64> for Matrix {
     }
 }
 
-impl Mul<Matrix> for Matrix {
-    type Output = Self;
-    fn mul(self, rhs: Matrix) -> Self::Output {
-        let mut out = Matrix::new(self.rows, rhs.cols);
-        let (lhs_cols, lhs_rows) = (self.cols, self.rows);
-        let rhs_cols = rhs.cols;
-        for out_row in 0..lhs_rows {
-            for out_col in 0..rhs_cols {
-                let out_index = out.get_index((out_row, out_col));
-                out.data[out_index] = (0..lhs_cols)
-                    .map(|i| self[(out_row, i)] * rhs[(i, out_col)])
-                    .sum();
+macro_rules! impl_mat_mul {
+    ($lhs:ty, $rhs:ty) => (
+        impl<'a> Mul<$rhs> for $lhs {
+            type Output = Result<Matrix, &'static str>;
+            fn mul(self, rhs: $rhs) -> Self::Output {
+                if self.cols != rhs.rows {
+                    return Err(
+                        "can't multiply left cols don't match right rows");
+                }
+                let mut out = Matrix::new(self.rows, rhs.cols);
+                let (lhs_cols, lhs_rows) = (self.cols, self.rows);
+                let rhs_cols = rhs.cols;
+                for out_row in 0..lhs_rows {
+                    for out_col in 0..rhs_cols {
+                        let out_index = out.get_index((out_row, out_col));
+                        out.data[out_index] = (0..lhs_cols)
+                            .map(|i| self[(out_row, i)] * rhs[(i, out_col)])
+                            .sum();
+                    }
+                }
+                Ok(out)
             }
         }
-        out
+    )
+}
+
+impl_mat_mul!(Matrix, Matrix);
+impl_mat_mul!(Matrix, &'a Matrix);
+impl_mat_mul!(&'a Matrix, &'a Matrix);
+impl_mat_mul!(&'a Matrix, Matrix);
+
+macro_rules! impl_mat_add {
+    ($lhs:ty, $rhs:ty) => (
+
+    impl<'a> Add<$rhs> for $lhs {
+        type Output = Matrix;
+        fn add(self, rhs: $rhs) -> Self::Output {
+            if self.cols != rhs.cols || self.rows != rhs.rows {
+                panic!("can't add these. rows and cols must be the same");
+            }
+            Matrix {
+                rows: self.rows,
+                cols: self.cols,
+                data: self.data
+                    .iter()
+                    .zip(rhs.data.iter())
+                    .map(|(&l, &r)| l + r)
+                    .collect(),
+            }
+        }
+    }
+
+    )
+}
+
+impl_mat_add!(Matrix, Matrix);
+impl_mat_add!(Matrix, &'a Matrix);
+impl_mat_add!(&'a Matrix, &'a Matrix);
+impl_mat_add!(&'a Matrix, Matrix);
+
+macro_rules! impl_mat_addassign {
+    ($rhs:ty) => (
+        impl<'a> AddAssign<$rhs> for Matrix {
+            fn add_assign(&mut self, rhs: $rhs) {
+                for i in 0..self.data.len() {
+                    self.data[i] += rhs.data[i];
+                }
+            }
+        }
+    )
+}
+
+impl_mat_addassign!(&'a Matrix);
+impl_mat_addassign!(Matrix);
+
+impl MulAssign<f64> for Matrix {
+    fn mul_assign(&mut self, rhs: f64) {
+        for i in 0..self.data.len() {
+            self.data[i] *= rhs;
+        }
+    }
+}
+
+impl<'a> Sub<&'a Matrix> for Matrix {
+    type Output = Self;
+    fn sub(self, rhs: &Matrix) -> Self::Output {
+        if self.cols != rhs.cols || self.rows != rhs.rows {
+            panic!("can't add these. rows and cols must be the same");
+        }
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: self.data
+                .iter()
+                .zip(rhs.data.iter())
+                .map(|(&l, &r)| l - r)
+                .collect(),
+        }
     }
 }
 
@@ -117,24 +232,6 @@ impl Sigmoid for Matrix {
     }
 }
 
-trait Transpose {
-    fn transpose(&self) -> Self;
-}
-
-impl Transpose for Matrix {
-    fn transpose(&self) -> Matrix {
-        let mut out = Matrix::new(self.cols, self.rows);
-        for row in 0..self.rows {
-            for col in 0..self.cols {
-                let out_index = out.get_index((col, row));
-                out.data[out_index] = self[(row, col)];
-            }
-        }
-        out
-    }
-}
-
-
 #[cfg(test)]
 mod describe_matrix {
     use super::*;
@@ -149,8 +246,6 @@ mod describe_matrix {
     #[test]
     fn it_can_test_for_equivalence() {
         let m1 = Matrix::new(2, 2);
-        let m2 = Matrix::new(2, 2);
-        assert_eq!(m1, m2);
         let m3 = Matrix::new_random(2, 2);
         assert!(m1 != m3);
     }
@@ -194,14 +289,103 @@ mod describe_matrix {
         assert_eq!(transposed[(1, 2)], 6.0);
     }
 
-    #[test]
-    fn it_can_calculate_dot_product() {
-        let m1 = Matrix::from(2, 3, &vec![1., 2., 3., 4., 5., 6.]);
-        let m2 = Matrix::from(3, 2, &vec![1., 2., 1., 2., 1., 2.]);
-        let dot = m1 * m2;
-        assert_approx_eq!(dot[(0, 0)], 6.);
-        assert_approx_eq!(dot[(0, 1)], 12.);
-        assert_approx_eq!(dot[(1, 0)], 15.);
-        assert_approx_eq!(dot[(1, 1)], 30.);
+    #[cfg(test)]
+    mod multiplying_two_matricies {
+        use super::*;
+
+        #[test]
+        fn it_can_calculate_dot_product() {
+            let m1 = Matrix::from(2, 3, &vec![1., 2., 3., 4., 5., 6.]);
+            let m2 = Matrix::from(3, 2, &vec![1., 2., 1., 2., 1., 2.]);
+            let dot = m1 * &m2;
+            assert_approx_eq!(dot[(0, 0)], 6.);
+            assert_approx_eq!(dot[(0, 1)], 12.);
+            assert_approx_eq!(dot[(1, 0)], 15.);
+            assert_approx_eq!(dot[(1, 1)], 30.);
+        }
+
+        #[test]
+        fn it_can_multiply_struct_and_struct() {
+            let m1 = Matrix::from(2, 2, &vec![2., 2., 2., 2.]);
+            let m2 = Matrix::from(2, 2, &vec![3., 3., 3., 3.]);
+            m1 * m2;
+        }
+
+        #[test]
+        fn it_can_multiply_struct_and_ref() {
+            let m1 = Matrix::from(2, 2, &vec![2., 2., 2., 2.]);
+            let m2 = &Matrix::from(2, 2, &vec![3., 3., 3., 3.]);
+            m1 * m2;
+        }
+
+        #[test]
+        fn it_can_multiply_ref_and_struct() {
+            let m1 = &Matrix::from(2, 2, &vec![2., 2., 2., 2.]);
+            let m2 = Matrix::from(2, 2, &vec![3., 3., 3., 3.]);
+            m1 * m2;
+        }
+
+        #[test]
+        fn it_can_multiply_ref_and_ref() {
+            let m1 = &Matrix::from(2, 2, &vec![2., 2., 2., 2.]);
+            let m2 = &Matrix::from(2, 2, &vec![3., 3., 3., 3.]);
+            m2 * m1;
+        }
+
+        #[test]
+        #[should_panic]
+        fn it_panics_if_the_matrices_arent_the_right_size() {
+            let m1 = Matrix::new(4, 2);
+            let m2 = Matrix::new(3, 5);
+            let out = m1 * &m2;
+        }
+    }
+
+    #[cfg(test)]
+    mod adding_two_matrices {
+        use super::*;
+
+        #[test]
+        fn it_can_add_two_matrices() {
+            let m1 = Matrix::from(2, 2, &vec![1., 2., 3., 4.]);
+            let m2 = Matrix::from(2, 2, &vec![1., 2., 1., 2.]);
+            let out = m1 + &m2;
+            assert_approx_eq!(out[(0, 0)], 2.);
+            assert_approx_eq!(out[(0, 1)], 4.);
+            assert_approx_eq!(out[(1, 0)], 4.);
+            assert_approx_eq!(out[(1, 1)], 6.);
+        }
+
+        #[test]
+        #[should_panic]
+        fn it_panics_if_the_matrices_arent_the_same_size() {
+            let m1 = Matrix::new(2, 2);
+            let m2 = Matrix::new(2, 3);
+            let out = m1 + &m2;
+        }
+    }
+
+    #[cfg(test)]
+    mod subtracting_two_matrices {
+        use super::*;
+
+        #[test]
+        fn it_can_subtract_two_matrices() {
+            let m1 = Matrix::from(2, 2, &vec![1., 2., 3., 4.]);
+            let m2 = Matrix::from(2, 2, &vec![1., 2., 1., 2.]);
+            let out = m1 - &m2;
+            assert_approx_eq!(out[(0, 0)], 0.);
+            assert_approx_eq!(out[(0, 1)], 0.);
+            assert_approx_eq!(out[(1, 0)], 2.);
+            assert_approx_eq!(out[(1, 1)], 2.);
+        }
+
+        #[test]
+        #[should_panic]
+        fn it_panics_if_the_matrices_arent_the_same_size() {
+            let m1 = Matrix::new(2, 2);
+            let m2 = Matrix::new(2, 3);
+            let out = m1 - &m2;
+        }
     }
 }
